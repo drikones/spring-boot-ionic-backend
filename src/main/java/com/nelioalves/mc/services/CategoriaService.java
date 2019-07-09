174647435c2b6db9f5e01ae1b65d1ec2aa5adb74
Adriano Rocha <adriano_rocha15@hotmail.com>
2019-07-09 13:10:13 -0300

    Criando operacao de instanciacao

--- src/main/java/com/nelioalves/mc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/mc/services/CategoriaService.java
@@ -8,21 +8,22 @@ import org.springframework.stereotype.Service;
 import com.nelioalves.mc.domain.Categoria;
 import com.nelioalves.mc.repositories.CategoriaRepository;
 
-
 /**
- * Classe de serviços da entidade categoria que acessam os métodos da minha camada de acesso a dados
+ * Classe de serviços da entidade categoria que acessam os métodos da minha
+ * camada de acesso a dados
  * 
  * @author Adriano Rocha
  * @since 09/07/2019
  */
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	/**
 	 * retorna uma categoria por id
+	 * 
 	 * @param Id
 	 * @return categoria
 	 */
@@ -30,7 +31,5 @@ public class CategoriaService {
 		Optional<Categoria> categoria = categoriaRepository.findById(Id);
 		return categoria.orElse(null);
 	}
-	
-	
 
 }
