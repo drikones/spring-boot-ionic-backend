7419b721674dcdb45f1aad841a88201c3a3c8d40
ADRIANO DA SILVA ROCHA <adriano_rocha15@hotmail.com>
2019-12-23 20:15:25 -0300

    Implementando autorizacao

--- src/main/java/com/nelioalves/mc/config/SecurityConfig.java
+++ src/main/java/com/nelioalves/mc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.nelioalves.mc.security.JWTAuthenticationFilter;
+import com.nelioalves.mc.security.JWTAuthorizationFilter;
 import com.nelioalves.mc.security.JWTUtil;
 
 /**
@@ -65,6 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
