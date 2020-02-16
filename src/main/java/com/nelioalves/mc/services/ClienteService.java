24eb0f9936f22e24fec2e6fce6df14ac2c873901
ADRIANO DA SILVA ROCHA <adriano_rocha15@hotmail.com>
2020-02-16 19:05:56 -0300

    Bonus: ajustando o tamanho da imagem

--- src/main/java/com/nelioalves/mc/services/ClienteService.java
+++ src/main/java/com/nelioalves/mc/services/ClienteService.java
@@ -58,6 +58,9 @@ public class ClienteService {
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
 	
+	@Value("${img.profile.size}")
+	private Integer size;
+	
 	/**
 	 * Busca o cliente por id
 	 * 
@@ -186,6 +189,8 @@ public class ClienteService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
 		String fileName = prefix + user.getId() + ".jpg";
 
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
